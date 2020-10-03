162c5f732232427cb80c7f1e11df7717896d19cc
Erick Saravia <erickjosue8916@gmail.com>
2020-10-03 17:02:42 -0600

    feet: (tasklist) show results in a table

--- objects/views/tasks/taskList.php
+++ objects/views/tasks/taskList.php
@@ -1,3 +1,24 @@
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Name</th>
+      <th scope="col">Status</th>
+      <th scope="col">Creatie_at</th>
+    </tr>
+  </thead>
+  <tbody>
 <?php
-
-var_dump($tasks);
+    for ($i=0; $i < count($tasks); $i++) { 
+     ?>
+      <tr>
+        <td><?=$tasks[$i]->id?></td>
+        <td><?=$tasks[$i]->name?></td>
+        <td><?=$tasks[$i]->status?></td>
+        <td><?=$tasks[$i]->create_at?></td>
+      </tr>
+     <?php
+    }
+?>
+  </tbody>
+</table>
