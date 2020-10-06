59baa16ee70b184468bbbf11dc7774fdb46a34ad
Erick Saravia <erickjosue8916@gmail.com>
2020-10-05 21:42:49 -0600

    feet: (style) add new style to template
    - 3 sections in a page
    - 90% with in container
    - navbar (active option menu with controller name)
    - roboto as font
    - set palete color

--- parcial/views/footer.php
+++ parcial/views/footer.php
@@ -1,8 +1,7 @@
 </div>
-<footer>
+    <footer class="is_jcontent_c is_acontent_c">
         <h3>Este es un footer</h3>
     </footer>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/js/bootstrap.bundle.min.js" integrity="sha384-BOsAfwzjNJHrJ8cZidOg56tcQWfp6y72vEJ8xQ9w6Quywb24iOsW913URv1IS4GD" crossorigin="anonymous"></script>
-
+    <script src="<?=BASE_DIR?>assets/js/script.js"></script>
 </body>
 </html>
--- parcial/views/header.php
+++ parcial/views/header.php
@@ -1,29 +1,33 @@
+<?php
+  $active = "Home";
+  if (isset($_REQUEST['controller'])){
+    $active = $_REQUEST['controller'];
+  }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?= BASE_DIR; ?>css/style.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css" integrity="sha384-DhY6onE6f3zzKbjUPRc2hOzGAdEf4/Dz+WJwBvEYL/lkkIsI3ihufq9hk9K4lVoK" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@1,300&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/3e11f034b2.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="<?= BASE_DIR; ?>assets/css/style.css">
+    
     <title>TPI -render con OOP-</title>
 </head>
 <body>
-    <header>
-      <nav class="p-2">
-        <ul class="nav nav-pills">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Active</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-          </li>
-        </ul>
-      </nav>
+    <header class="navbar">
+      <div class="navbar__brand is_jcontent_c is_acontent_c">
+        <h1>Parcial</h1> 
+      </div>
+  
+      <div class="navbar__menu is_jitems_r is_acontent_c">
+        <div>
+          <a href="" class="btn <?=($active == "Home") ? "active": ""; ?>">home</a>
+          <a href="" class="btn <?=($active == "About") ? "active": ""; ?>">about</a>
+          <a href="" class="btn <?=($active == "Contact") ? "active": ""; ?>">contact</a>
+          <a href="" class="btn <?=($active == "Component") ? "active": ""; ?>">test</a>
+        </div>
+      </div>
     </header>
     <div class="container">
